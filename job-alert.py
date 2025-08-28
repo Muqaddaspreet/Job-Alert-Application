e61468b8438b4b400c354171bc342a30f74013b2
Muqaddaspreet Singh <54834880+Muqaddaspreet@users.noreply.github.com>
2025-08-28 08:35:12 -0400

    Update job-alert.py

--- job-alert.py
+++ job-alert.py
@@ -7,6 +7,8 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import time
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # GraphQL endpoint
 url = "https://e5mquma77feepi2bdn4d6h3mpu.appsync-api.us-east-1.amazonaws.com/graphql"
